52ebbafa5207e90e4313599d230e825b8a7684dd
Simon1130 <3075302l@student.gla.ac.uk>
2026-05-06 21:24:10 +0100

    define some calculator simple functions
    addition, subtraction, multiplication, division, square, square root, x square

---
+++ calculator_backend.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 18:26:32 2026
+
+@author: simon
+"""
+
+class Calculater:
+    
+    def addition(x,y):
+        return x + y
+        
+    def subtraction(x,y):
+        return x - y
+    
+    def multiplication(x,y):
+        return x * y
+    
+    def Division(x,y):
+        return x / y
+    
+    def square(x):
+        return x ** 2
+    
+    def square_root(x):
+        return x ** 0.5
+    
+    def x_square(x,y): #y is the index
+        return x ** y
